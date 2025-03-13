c3974ebd619af0161cbf51b30df6d365c3ab31e0
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-13 23:50:49 +0600

    finally done show and hide pssword

--- my-project/src/component/Login.jsx
+++ my-project/src/component/Login.jsx
@@ -2,61 +2,69 @@ import React, { useContext, useState } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
 const Login = () => {
-    const navigate= useNavigate();
-    const {signInUser,signInWithGoogle}=useContext(AuthContext);
+    const navigate = useNavigate();
+    const { signInUser, signInWithGoogle } = useContext(AuthContext);
 
-    const [showPassword,setShowPassword]= useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-    const handleLogin=(e)=>{
+    const handleLogin = (e) => {
         e.preventDefault();
-        const email=e.target.email.value;
-        const password=e.target.password.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
 
         console.log(email, password);
 
-        signInUser(email,password)
-        .then(result=>{
-            console.log(result.user)
-            e.target.reset();
-            navigate('/profile')
-        })
-        .catch(error=>{
-            console.log("Error",error)
-        })
+        signInUser(email, password)
+            .then(result => {
+                console.log(result.user)
+                e.target.reset();
+                navigate('/profile')
+            })
+            .catch(error => {
+                console.log("Error", error)
+            })
     }
 
 
 
-    const handleGoogle=()=>{
+    const handleGoogle = () => {
         signInWithGoogle()
-        .then(result=>{
-            console.log(result)
-            navigate('/profile')
-            setShowPassword(true);
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result)
+                navigate('/profile')
+                setShowPassword(true);
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
 
     return (
         <div className="">
             <div className="hero-content flex-col">
-            <h1 className="text-2xl font-bold">Login now!</h1>
-                
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <h1 className="text-2xl font-bold">Login now!</h1>
+
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
                     <form onSubmit={handleLogin} className="card-body relative">
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Email</label>
                             <input type="email" name='email' className="input" placeholder="Email" />
 
                             <label className="fieldset-label">Password</label>
-                            <input type={showPassword ? 'text': 'password'} name='password' className="input" placeholder="Password" />
+                            <input type={showPassword ? 'text' : 'password'} name='password' className="input" placeholder="Password" />
 
-                            <button type='button' onClick={()=>setShowPassword(!showPassword)} className='absolute right-12 top-32 text-xl'><FaEyeSlash /></button>
+                            <button type='button' onClick={() => setShowPassword(!showPassword)} className='absolute right-12 top-32 text-xl'>
+                                {
+                                    showPassword ? 
+                                    <FaEye /> 
+                                    : 
+                                    <FaEyeSlash />
+                                }
+                            </button>
 
                             <div><a className="link link-hover">Forgot password?</a></div>
 
