b018462bf7616a2ec2fb220edd9bd4078cd064bd
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-13 22:30:34 +0600

    show password star

--- my-project/src/component/Login.jsx
+++ my-project/src/component/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Link, useNavigate } from 'react-router-dom';
+import { FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
     const navigate= useNavigate();
@@ -42,13 +43,15 @@ const Login = () => {
             <h1 className="text-2xl font-bold">Login now!</h1>
                 
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form onSubmit={handleLogin} className="card-body">
+                    <form onSubmit={handleLogin} className="card-body relative">
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Email</label>
                             <input type="email" name='email' className="input" placeholder="Email" />
 
                             <label className="fieldset-label">Password</label>
                             <input type="password" name='password' className="input" placeholder="Password" />
+
+                            <button className='absolute right-12 top-32 text-xl'><FaEyeSlash /></button>
 
                             <div><a className="link link-hover">Forgot password?</a></div>
 
